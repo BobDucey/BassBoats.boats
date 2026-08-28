bc82c3c3b718e7b5af5d9cdf175f05eb8d31cf64
BobDucey <rpducey@gmail.com>
2026-08-28 10:21:51 -0400

    Update visit.js

--- api/visit.js
+++ api/visit.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
     const supabaseUrl = process.env.SUPABASE_URL;
     const serviceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
     if (!supabaseUrl || !serviceKey) {
-      return res.status(200).json({ ok: false });
+      return res.status(200).json({ ok: false, debug: 'missing_env', hasUrl: !!supabaseUrl, hasKey: !!serviceKey });
     }
 
     const resp = await fetch(`${supabaseUrl}/rest/v1/page_views`, {
